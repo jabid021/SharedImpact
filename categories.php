95a6bd9c87c96bb19da73c033b109d44e6969996
Jordan <jordanabid@gmail.com>
2024-04-01 22:39:11 +0200

    Avancement page categorie.php template 85% done

--- categories.php
+++ categories.php
@@ -4,28 +4,34 @@ echo "<main>";
 include("navbar.php");
 ?>
 
-  <div id="content-left">
-    <input type="button" class="btn-blue-advice" value="Create a new Category >">
+<div id="content-left">
+  <input type="button" class="btn-blue-advice" value="Create a new Category >">
 
-    <form action="post.php" method="POST">
-      <input type="hidden" action="add-category">
-      <div class="div-form">
-        <div class="input-tooltip">NEW CATEGORY</div>
-        <input align="right" type="text"  name="category" class="black-btn-confirm" placeholder="CATEGORY TITLE">
-      </div>
-      <div align="right"><input type="submit" value="Save >"></div>
-    </form>
-  </div>
+  <form action="post.php" method="POST">
+    <input type="hidden" action="add-category">
+    <div class="div-form">
+      <div class="input-tooltip">NEW CATEGORY</div>
+      <input align="right" type="text"  name="category" class="black-btn-confirm" placeholder="CATEGORY TITLE">
+    </div>
+    <div align="right"><input class="btn-submit" type="submit" value="Save >"></div>
+  </form>
+</div>
 
-  <div id="content-right">
-    <input type="button" class="blue-btn-advice" value="Select a Category >">
+<div id="content-right">
+  <input type="button" class="btn-blue-advice" value="Select a Category >">
+  <div class="div-form">
+    <div class="input-tooltip">UPDATE A CATEGORY</div>
     <select id="selectCategory">
       <option value="1">Education</option>
       <option value="2">Employment</option>
       <option value="3">Health/Mental Health</option>
     </select>
+  </div>
 
-    <input onclick="redirectCateg('update')" id="update-category" type="button" value="Modify category"><input id="delete-category" onclick="redirectCateg('delete')" type="button" value="Delete category">
+  <div id="btn-row">
+    <input onclick="redirectCateg('update')" id="update-category" class="btn-submit" type="button" value="Modify category"><input class="btn-submit" id="delete-category" onclick="redirectCateg('delete')" type="button" value="Delete category">
+  </div>
+
   </div>
 </main>
 
